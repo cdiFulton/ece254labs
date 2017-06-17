e51e6fe5a8d1c6009c8ae876eeb1c07a86d9b6d4
Chris <cdifulton@gmail.com>
2017-06-17 14:07:04 -0400

    lab2 done

--- lab2/starter/RTX_CM3/SRC/CM/rt_Task_ext.c
+++ lab2/starter/RTX_CM3/SRC/CM/rt_Task_ext.c
@@ -71,11 +71,11 @@ OS_RESULT rt_tsk_get (OS_TID task_id, RL_TASK_INFO *p_task_info) {
     
     if (p_task_info->state == RUNNING) 
     {
-        p_task_info->stack_usage = ((U16)os_stackinfo + rt_get_PSP())/(*temp->stack);
+        p_task_info->stack_usage = 100*((U16)os_stackinfo - rt_get_PSP())/(*temp->stack);
     }
     else
     {
-        p_task_info->stack_usage = ((U16)os_stackinfo + temp->tsk_stack)/(*temp->stack);
+        p_task_info->stack_usage = 100*((U16)os_stackinfo - temp->tsk_stack)/(*temp->stack);
     }
     
     p_task_info->ptask       = temp->ptask;
